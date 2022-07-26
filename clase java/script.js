ecebd93a09b33085f77fd08628af84dd75ba6130
DiazFernando <fernandodiaz@tdf.edu.ar>
2022-07-26 00:14:03 -0300

    Ejercicio java entregable

--- clase java/script.js
+++ clase java/script.js
@@ -1,12 +1,32 @@
-let btn_nombre = document.getElementById("btn_nombre");
-btn_nombre.addEventListener("click",mostrar);
+let btn_nombreCompleto = document.getElementById("btn_nombreCompleto");
+btn_nombreCompleto.addEventListener("click",saludar,obtenerDatos);
+btn_nombreCompleto.addEventListener("click",obtenerDatos);
 
-let nombre = "anonimo"
+let nombre = "anonimo";
+let apellido = "anonimo";
+let edad = 0;
 
-function mostrar() {
+function saludar() {
     let nombre = "anonimo";
+    let apellido = "anonimo";
     let inputNombre = document.getElementById("inputNombre");
+    let inputApellido = document.getElementById("inputApellido");
     nombre = inputNombre.value;
+    apellido = inputApellido.value;
 
-    alert("Bienvenido " + nombre);
+    alert("Bienvenido " + nombre + " " + apellido);
+}
+
+function obtenerDatos() {
+    let nombre = "anonimo";
+    let apellido = "anonimo";
+    let edad = 0;
+    let inputNombre = document.getElementById("inputNombre");
+    let inputApellido = document.getElementById("inputApellido");
+    let inputEdad = document.getElementById("inputEdad");
+    nombre = inputNombre.value;
+    apellido = inputApellido.value;
+    edad = inputEdad.value;
+
+    console.log(nombre, apellido, edad);
 }
