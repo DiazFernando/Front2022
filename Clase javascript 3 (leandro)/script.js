7916979465f672b80f7c5b10250d54503bc74446
DiazFernando <fernandodiaz@tdf.edu.ar>
2022-07-29 20:48:58 -0300

    ejercicio agregar terminado

--- Clase javascript 3 (leandro)/script.js
+++ Clase javascript 3 (leandro)/script.js
@@ -3,6 +3,9 @@ let boton = document.getElementById("btn_agregar");
 let input = document.getElementById("inputLista");
 
 function agregarTarea (){
-   let elemento = document 
+   let elemento = document.createElement("li");
+   elemento.innerHTML = inputLista.value;
+   lista.appendChild(elemento); 
 }
+
 
