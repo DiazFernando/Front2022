5fe374a9b0af20bf4da3b8a434930ac2f917d7f5
DiazFernando <fernandodiaz@tdf.edu.ar>
2022-07-29 20:41:09 -0300

    ejercicio agregar elemento

---
+++ Clase javascript 3 (leandro)/script.js
@@ -0,0 +1,8 @@
+let lista = document.getElementById("lista");
+let boton = document.getElementById("btn_agregar");
+let input = document.getElementById("inputLista");
+
+function agregarTarea (){
+   let elemento = document 
+}
+
