885bc5a8f5792a175778a238939441279ce22ac5
DiazFernando <fernandodiaz@tdf.edu.ar>
2022-08-12 22:51:06 -0300

    se corrigio examen

--- Examen 12.08/ejercicio1.js
+++ Examen 12.08/ejercicio1.js
@@ -3,6 +3,7 @@ let primerNumero = document.getElementById("num1");
 
 let segundoNumero = document.getElementById("num2");
 
+
 let botonSumar = document.getElementById("suma");
 botonSumar.addEventListener("click", sumar);
 
@@ -14,19 +15,17 @@ mostrarResultado= document.querySelector(".resultado");
 
 function sumar(){
 
-    let resultado = parseInt(num1.value) + parseInt(num2.value);
-    console.log(resultado);
+    let resultado = parseInt(primerNumero.value) + parseInt(segundoNumero.value);
     let mostrar = document.createElement("p");
-    mostrar.innerHTML = "la suma de " + num1.value + " + " + num2.value + " = " + resultado;
+    mostrar.innerHTML = "la suma de " + primerNumero.value + " + " + segundoNumero.value + " = " + resultado;
     mostrarResultado.appendChild(mostrar);
 }
 
 
 function restar(){
-    let resultado = parseInt(num1.value) - parseInt(num2.value);
-    console.log(resultado)
+    let resultado = parseInt(primerNumero.value) - parseInt(segundoNumero.value);
     let mostrar = document.createElement("p");
-    mostrar.innerHTML = "la resta de " + num1.value + " - " + num2.value + " = " + resultado;
+    mostrar.innerHTML = "la resta de " + primerNumero.value + " - " + segundoNumero.value + " = " + resultado;
     mostrarResultado.appendChild(mostrar);
 }
 
