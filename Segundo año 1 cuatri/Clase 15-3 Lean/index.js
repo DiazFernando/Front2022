4978516d3813d8cf33f2fbcf705a4fabae708792
Diaz Fernando <fernandodiaz@tdf.edu.ar>
2023-03-15 20:53:55 -0300

    Segunda clase

---
+++ Segundo año 1 cuatri/Clase 15-3 Lean/index.js
@@ -0,0 +1,40 @@
+//Se creo un objeto
+
+/*let persona = {
+    nombre: "Lucas",
+    apellido: "Martinéz",
+    edad: 47,
+    sexo: 'M',
+    dni: 34483889,
+    hijos: ["Ana","Marcos"]
+}
+
+console.log(persona.nombre);
+
+let persona2 = persona;
+persona2.nombre = "Alicia";
+
+console.log(persona2.nombre);
+*/
+
+
+let data ={
+    "empresa":"TDF",
+    "empleados" : [
+        {
+            "nombre":"Fernando",
+            "apellido":"Diaz",
+            "dni":"11111111"
+        },
+        {
+            "nombre":"Maximiliano",
+            "apellido":"Fernandez",
+            "dni":"22222222"
+        },
+        {
+            "nombre":"Luis",
+            "apellido":"Zapata",
+            "dni":"33333333"
+        }
+    ]
+}
