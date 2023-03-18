435c3d9478c1c86f78f727d5d29b19046436605b
Diaz Fernando <fernandodiaz@tdf.edu.ar>
2023-03-18 17:36:16 -0300

    archivo JSON a objeto JS

---
+++ Segundo año 1 cuatri/Clase 15-3 Lean/json.js
@@ -0,0 +1,60 @@
+//archivo JSON a objeto Js
+
+let data = `{
+    "empresa":"TDF",
+    "empleados":[
+        {
+            "nombre":"Fernando",
+            "apellido":"Diaz"
+        },
+        {
+            "nombre":"Luis",
+            "apellido":"Zapata"
+        },
+        {
+            "nombre":"David",
+            "apellido":"Fernandez"
+        }
+    ]
+}`
+
+let datos = JSON.parse(data);
+//"consulto los datos JSON obtenidos")
+console.log(datos);
+
+console.log("----------");
+
+//"consulto los empleados obtenidos del JSON"
+console.log(datos.empleados);
+
+console.log("----------");
+
+//"consulto al empleado en posición 1 obtenidos del JSON"
+console.log(datos.empleados[1]);
+
+console.log("----------");
+
+//Objeto Js a archivo JSON
+
+let personas = [{
+    nombre :"Sofia",
+    apellido : "Villalba",
+    edad : 29,
+    dni :1111111
+},
+{
+    nombre :"Bernardo",
+    apellido : "Fiori",
+    edad : 33,
+    dni :22222222
+},
+{
+    nombre :"Mariana",
+    apellido : "Orzusa",
+    edad : 50,
+    dni :3333333
+}];
+
+let dataJSON = JSON.stringify(personas);
+
+console.log(dataJSON);
