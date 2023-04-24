494535bdc099d069712761d17fe2044440813da0
Diaz Fernando <fernandodiaz@tdf.edu.ar>
2023-04-23 23:49:17 -0300

    estilo para el form

--- Segundo año 1 cuatri/ejercicios/src/componentes/Forms/MyForms.jsx
+++ Segundo año 1 cuatri/ejercicios/src/componentes/Forms/MyForms.jsx
@@ -1,5 +1,8 @@
-import { useState } from "react"
+import { useState } from "react";
 import Card from "../Card/Card";
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import './MyForms.css';
 
 const MyForm = () => {
     const [nombre,setNombre] = useState ('');
@@ -24,29 +27,42 @@ const MyForm = () => {
 
     return(
         <>
-         <div className="form">
+         <div className="content">
             <form onSubmit={handleSubmit}>
-                <label>Nombre: 
+                <div>
+                <label>Nombre:  
                     <input type="text" pattern="[A-Za-z]*" value={nombre} onChange={(e)=>{setNombre(e.target.value)}} placeholder="Nombre"/>
                 </label>
+                </div>
 
+                <div>
                 <label>Apellido: 
                     <input type="text" pattern="[A-Za-z]*" value={apellido} onChange={(e)=>{setApellido(e.target.value)}} placeholder="Apellido"/>
                 </label>
-
+                </div>
+                
+                <div>
                 <label>DNI: 
                     <input type="text" pattern="[0-9]*" value={dni} onChange={(e)=>{setDni(e.target.value)}} placeholder="DNI"/>
                 </label>
-
+                </div>
+                
+                <div>
                 <label>Ciudad: 
                     <input type="text" pattern="[A-Za-z]*" value={ciudad} onChange={(e)=>{setCiudad(e.target.value)}} placeholder="Ciudad"/>
                 </label>
-
+                </div>
+                
+                <div>
                 <label>Comentario: 
                     <textarea type="text" value={comentario} onChange={(e)=>{setComentario(e.target.value)}} placeholder="Deja tu comentario aqui"/>
                 </label>
+                </div>
 
+                <div className="buttonEnviar">
                 <button type="submit">Enviar</button>
+                </div>
+                
             </form>  
             <div>
                 {personas.map((persona)=> <Card key={persona.nombre} nombre={persona.nombre} apellido={persona.apellido} dni={persona.dni} ciudad={persona.ciudad} comentario={persona.comentario}/>)}
